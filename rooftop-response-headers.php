a4c6bc48f72709add15733f72a816f502f45e1c6
Paul Hendrick <paul@errorstudio.co.uk>
2016-01-24 15:04:19 +0000

    add the current page and per-page headers to collection responses

--- rooftop-response-headers.php
+++ rooftop-response-headers.php
@@ -89,6 +89,11 @@ class Rooftop_Response_Headers {
                 $this->rooftop_set_headers_for_collection();
             }
 
+            $page = $request['page'] ? (int)$request['page'] : 1;
+            $per_page = $request['per_page'] ? (int)$request['per_page'] : 10;
+            $response->header( 'X-WP-Page', $page );
+            $response->header( 'X-WP-Per-Page', $per_page );
+
             return $response;
         }, 99, 3);
     }
